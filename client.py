d3a46c50fc4318d4719b51071a93debd7398f861
George Paci <george.paci@excella.com>
2019-07-16 20:16:43 -0400

    Client and server that interact via mouse clicks: colorful lines

---
+++ client.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import pygame
+import socket
+import time
+
+BLUE=pygame.Color(0,0,255)
+
+def log(level, *rest):
+    if VERBOSITY >= level:
+        print('client: ', *rest)
+
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 640, 480
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.DOUBLEBUF) # pygame.HWSURFACE | 
+        self._running = True
+ 
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+            return
+        if event.type != pygame.MOUSEMOTION:
+            log(1, event, event.type)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            msg = bytes('MOUSEBUTTONDOWN %s %s' % (event.pos, event.button), 'ascii')
+            s.send(msg)
+            z = s.recv(5)  # Warning: optimistic!
+            rsize = (z[0]*256 + z[1])*(z[2]*256 + z[3])*z[4]
+            data = self.receive_all(s, rsize)
+            self.display_data(z, data)
+            log(2, "Received: %d %s" % (len(data), data[:100]))
+
+    def receive_all(self, s, rsize):
+        r = []
+        while len(r) < rsize:
+            r += s.recv(rsize)
+        return bytes(r)
+
+
+    def on_loop(self):
+        pass
+    def on_render(self):
+        pass
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def display_data(self, z, data):
+        log(2, "z has %d bytes" % len(z))
+        width = z[0]*256 + z[1]
+        height = z[2]*256 + z[3]
+        color_mode = z[4]
+        log(1, "width: %d  height: %d  color_mode: %d" % (width, height, color_mode))
+        buf = self._display_surf.get_buffer()
+        #self._display_surf.fill(pygame.Color(data[0],data[0],data[0]), (0,0,width,height))
+        buf.write(data)
+        self._display_surf.unlock()
+        pygame.display.update()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == '__main__':
+    HOST, PORT = "localhost", 9999
+    VERBOSITY = 0
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    theApp = App()
+    theApp.on_execute()
+
+"""Event types:
+QUIT              none
+ACTIVEEVENT       gain, state
+
+KEYDOWN           unicode, key, mod
+KEYUP             key, mod
+
+MOUSEBUTTONDOWN   pos, button
+MOUSEBUTTONUP     pos, button
+MOUSEMOTION       pos, rel, buttons
+
+JOYAXISMOTION     joy, axis, value
+JOYBALLMOTION     joy, ball, rel
+JOYHATMOTION      joy, hat, value
+JOYBUTTONUP       joy, button
+JOYBUTTONDOWN     joy, button
+
+VIDEORESIZE       size, w, h
+VIDEOEXPOSE       none
+
+USEREVENT         code
+"""
--- server.py
+++ server.py
@@ -1,18 +1,77 @@
 #!/usr/bin/env python3
 
 import socketserver
+import random
+
+current = 128
+color_offset = 3 # 3=blue, 2=green, 1=red
+
+def log(level, *rest):
+    if VERBOSITY >= level:
+        print('server: ', *rest)
+
+def draw_random(databytes, x, y, d, current):
+    for p in range(x*y):
+        databytes[p*d+1] = random.randint(0,255)
+        databytes[p*d+3] = current
+
+def draw_lines(databytes, x, y, d, current):
+    for j in range(y):
+        for i in range(0,x,max(current,1)):
+            databytes[(j*x + i)*4+color_offset] = 255
+
 
 class TheRequestHandler(socketserver.BaseRequestHandler):
     """ TCP Request handler """
 
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print("{} sent:".format(self.client_address[0]))
-        print(self.data)
-        self.request.sendall("ACK from TCP server".encode())
+        self.width, self.height = 640, 480
+        self.depth = 4  # in bytes
+        while True:
+            self.data = self.request.recv(1024).strip()
+            if (len(self.data) == 0):
+                break
+            # logging:
+            log(2, "{} sent:".format(self.client_address[0]))
+            log(2, self.data)
+            self.handle_event(self.data)
+            # response:
+            image = self.make_image()
+            #log(3, "self.request class:", type(self.request))
+            #log(3, "dir self.request:", dir(self.request))
+            self.request.sendall(image)
+
+    def handle_event(self, data):
+        if data.startswith(b'MOUSEBUTTONDOWN'):
+            # MOUSEBUTTONDOWN (34, 41) 1
+            x = int(data.split()[1][1:-1])
+            y = int(data.split()[2][:-1])
+            global current
+            if x < self.width/3:
+                current -= 16
+            elif x > 2*self.width/3:
+                current += 16
+            elif y < self.height/3:
+                global color_offset
+                color_offset = (color_offset % 3) + 1
+            current = current % 256
+
+    def make_image(self):
+        # black image:
+        # 256 x 256, grayscale: [ 1, 0, 1, 0, 1]
+        x,y,d = self.width, self.height, self.depth
+        size = [ (x//256), (x%256), (y//256), (y%256), d]
+        log(1, size)
+        # So we're sending ARGB. Strange.
+        #databytes = [0, 0, 0, current] * (x * y)
+        databytes = [0] * (x*y*d)
+        draw_lines(databytes, x, y, d, current)
+        log(2, "current is now ", current)
+        return bytearray(size + databytes)
 
 if __name__ == '__main__':
     HOST, PORT = "localhost", 9999
+    VERBOSITY = 0
     tcp_server = socketserver.TCPServer((HOST, PORT), TheRequestHandler)
     tcp_server.serve_forever()
 
